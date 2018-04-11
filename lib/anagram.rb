fa98dc8a82c1a3723e852e7aded80e51e70fb5a0
Ally Netzel <netzelally@gmail.com>
2018-04-11 12:54:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
+require "pry"
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+ 
+def initialize(a_word)
+  @a_word = a_word
+end
+
+def match(words)
+  words.map do |word|
+    input = word.split("").sort
+    matches = @a_word.split("").sort
+      
+    if input.join("") == matches.join("")
+      return [input.join("")].sort
+    else
+      return []
+    end
+  end
+end
+end
