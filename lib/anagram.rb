e6706ba6399ea2fbfc17a7a45de13fcb27e353b8
Ally Netzel <netzelally@gmail.com>
2018-04-11 13:12:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
+require "pry"
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+ 
+def initialize(a_word)
+  @a_word = a_word
+end
+
+def match(words)
+  array = []
+  words.map do |word|
+    input = word.split("")
+    matches = @a_word.split("")
+      
+    if input.sort == matches.sort
+       array << input.join("")
+   end
+  end
+  array
+  end
+end
